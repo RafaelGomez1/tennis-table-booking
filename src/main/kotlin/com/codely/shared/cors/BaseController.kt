ac1352acda0afe5785d241b256a1ccb2c40a8be0
rgomez <rafael.gomez@code.seat>
2024-12-17 15:28:06 +0100

    Allow all origins

--- src/main/kotlin/com/codely/shared/cors/BaseController.kt
+++ src/main/kotlin/com/codely/shared/cors/BaseController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod
     allowCredentials = "true",
     originPatterns = ["*"],
     allowedHeaders = ["*"],
+    origins = ["*"],
     methods = [
         RequestMethod.POST,
         RequestMethod.OPTIONS,

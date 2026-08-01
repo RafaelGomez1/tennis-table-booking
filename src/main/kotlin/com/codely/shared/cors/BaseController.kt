ea9375f60e189fa89875808c2600289a8f7c7765
GOMEZRX44 <rafa.gomez@abbott.com>
2026-08-01 18:40:13 +0200

    Change CorsConfig to global web filter

--- src/main/kotlin/com/codely/shared/cors/BaseController.kt
+++ src/main/kotlin/com/codely/shared/cors/BaseController.kt
@@ -1,18 +1,3 @@
 package com.codely.shared.cors
 
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RequestMethod
-
-@CrossOrigin(
-    allowCredentials = "true",
-    originPatterns = ["*"],
-    allowedHeaders = ["*"],
-    methods = [
-        RequestMethod.POST,
-        RequestMethod.OPTIONS,
-        RequestMethod.GET,
-        RequestMethod.PATCH,
-        RequestMethod.DELETE
-    ]
-)
 open class BaseController
---
+++ src/main/kotlin/com/codely/shared/cors/CorsConfig.kt
@@ -0,0 +1,27 @@
+package com.codely.shared.cors
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val config = CorsConfiguration().apply {
+            allowedOriginPatterns = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+
+        return CorsWebFilter(source)
+    }
+}

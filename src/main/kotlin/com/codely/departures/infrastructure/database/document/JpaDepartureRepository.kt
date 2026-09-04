14c2c87eedbf91e767214a64902f59d0431197c8
GOMEZRX44 <rafa.gomez@abbott.com>
2026-09-04 23:34:44 +0200

    ADD @Repository

--- src/main/kotlin/com/codely/departures/infrastructure/database/document/JpaDepartureRepository.kt
+++ src/main/kotlin/com/codely/departures/infrastructure/database/document/JpaDepartureRepository.kt
@@ -1,5 +1,7 @@
 package com.codely.departures.infrastructure.database.document
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface JpaDepartureRepository : MongoRepository<DepartureDocument, String>
